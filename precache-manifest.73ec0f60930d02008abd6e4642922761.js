666ce3b13f0d1e60f627e9cd0697163c460b5308
Blink <tipsyblink11@gmail.com>
2022-01-09 16:17:05 -0800

    Updates

---
+++ precache-manifest.73ec0f60930d02008abd6e4642922761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "214b9617bf1b65e9eb71",
+    "url": "/todoapp/static/css/main.094f6de8.chunk.css"
+  },
+  {
+    "revision": "214b9617bf1b65e9eb71",
+    "url": "/todoapp/static/js/main.214b9617.chunk.js"
+  },
+  {
+    "revision": "72a8a52aa6952d21f7a9",
+    "url": "/todoapp/static/js/1.72a8a52a.chunk.js"
+  },
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "91a26a3ab4fb7869c868586d59b64363",
+    "url": "/todoapp/index.html"
+  }
+];
